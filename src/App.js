0cb7454e015a53627aa4e7ae45b987a6aed4bf82
Fede Risetti <115749116+risettifede@users.noreply.github.com>
2023-03-23 19:44:19 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import { Testimonio } from './components/Testimonio'; // EXPORTACIÓN NOMBRADA. 
 function App() {
   return (
     <div className="App">
-    <div className="contenedor-principal">
-      <h1>
-      BACK TO THE FUTURE: LAS OPINIONES DE LOS PROTAGONISTAS
-      </h1>
-    <Testimonio />
-    </div>
+      <div className="contenedor-principal">
+        <h1>
+          BACK TO THE FUTURE: LAS OPINIONES DE LOS PROTAGONISTAS
+        </h1>
+        <Testimonio
+          nombre="Marty McFly"
+          pais="EE.UU"
+          imagen="Marty"
+          rol="Viajero en el tiempo"
+          pelicula="Back to the future - Parte I II y III"
+          testimonio="Viajar en el tiempo es lo más loco que he hecho. Gracias al Doc he podido ver frente a frente a mis padres de adolescentes, ver cómo eran los autos (y las patinetas) del futuro. ¡Hasta pude ver un tren a carbón desaparecer en el aire y transportarse! Sin dudas, haber tenido que lograr que mi madre no se encamore de mí, fue la parte más dificil de mantener mi existencia en este planeta."
+          />
+      </div>
     </div>
   );
 }
--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "../style-sheets/Testimonio.css"
 
-export function Testimonio() {
+
+export function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <img className="imagen-testimonio"
-        src={require('../images/testimonio-Marty.png')}
+        src={require(`../images/Testimonio de Personajes/testimonio-${props.imagen}.png`)}
         alt="Foto de Marty" />
-      <div className="contenedor-texto-testimonio" >
-        <p className="nombre-testimonio">Marty McFly</p>
-        <p className="cargo-testimonio">Viajero en el tiempo</p>
-        <p className="parrafo-testimonio">"Viajar en el tiempo es lo más loco que he hecho. Gracias al Doc he podido ver a mis padres de adolescentes y ¡evitar que mi madre se enamore de mí! Increíble"</p>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+        <p className="rol-testimonio">{props.rol} en {props.pelicula}</p>
+        <p className="parrafo-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
   );
